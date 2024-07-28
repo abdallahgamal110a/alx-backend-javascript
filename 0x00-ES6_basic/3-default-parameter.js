9f81b50a3c2e95e91b3344daeffafba777f232be
abdallahgamal110a <abdallahgamal840@gmail.com>
2024-07-28 14:39:27 +0300

    Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,13 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    if (expansion1989 === undefined) {
-      expansion1989 = 89;
-    }
-  
-    if (expansion2019 === undefined) {
-      expansion2019 = 19;
-    }
     return initialNumber + expansion1989 + expansion2019;
   }
   
